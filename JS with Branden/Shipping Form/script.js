452ce0403774af76c17cc60fd75cfb9df21f29e9
Te El <vladislavpe4@gmail.com>
2024-07-29 13:28:06 +0900

    commit

--- JS with Branden/Shipping Form/script.js
+++ JS with Branden/Shipping Form/script.js
@@ -23,29 +23,32 @@ document.addEventListener("DOMContentLoaded", function(){
         let Date = e.target.elements["date"].value
         let Email = e.target.elements["email"].value
         console.log(FirstName)
-        verifyFormData(FirstName, SecondName, Zipcode, City, Date, Email))
         //////////////////////////////////////////////
+
+        function verifyFormData(FirstName, SecondName, Zipcode, City, Date, Email){
+            let errors = []
+                if(FirstName.length > 10 || FirstName === ""){
+                    // return an error message
+                    console.log(FirstName + "error")
+                    errors.push(FirstName)
+                }
+                if(SecondName.length > 10 || SecondName === ""){
+                    // return an error message
+                    console.log(SecondName + "error")
+                    errors.push(SecondName)
+                }
+                if (Zipcode === "" || isNaN(Zipcode) || Zipcode.length != 5){
+                    // return
+                    console.log(Zipcode + "error")
+                    errors.push(Zipcode)
+                }
+                if(City.length < 10){
+                    // return
+                    console.log(City + "error")
+                    errors.push(City)
+                }
+                return errors
+            }
     })
-    function verifyFormData(FirstName, SecondName, Zipcode, City, Date, Email){
-            if(FirstName.length > 10 || FirstName === ""){
-                // return an error message
-                console.log(FirstName + "error")
-                errors.push(FirstName)
-            }
-            if(SecondName.length > 10 || SecondName === ""){
-                // return an error message
-                console.log(SecondName + "error")
-                errors.push(SecondName)
-            }
-            if (Zipcode === "" || isNaN(Zipcode) || Zipcode.length != 5){
-                // return
-                console.log(Zipcode + "error")
-                errors.push(Zipcode)
-            }
-            if(City.length < 10){
-                // return
-                console.log(City + "error")
-                errors.push(City)
-            }
-        }
-    return errors
+})
+
