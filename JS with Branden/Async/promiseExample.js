5c1c5fea0f4eb62768729a5af7310492fd3c8ba5
Te El <vladislavpe4@gmail.com>
2024-07-30 16:18:43 +0900

    commit

---
+++ JS with Branden/Async/promiseExample.js
@@ -0,0 +1,26 @@
+
+// function getPromise(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const randomNumber = Math.random()
+
+//             console.log(randomNumber)
+
+//             if( randomNumber < 0.5){
+//                 resolve("Promise Resolved")
+//             } else{
+//                 reject("Promise Rejected")
+//             }
+
+//         }, 2000)
+//     })
+// }
+
+// getPromise().then(result => {
+//     console.log(result)
+
+// }).catch(error =>{
+//     console.log("There was an error")
+// })
+
+// console.log("This is after we get")
