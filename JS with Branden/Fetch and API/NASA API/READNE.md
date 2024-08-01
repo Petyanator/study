1ea893e73201d1e3d8ebb8676ab339405f175ca3
Te El <vladislavpe4@gmail.com>
2024-08-01 13:35:55 +0900

    commit

---
+++ JS with Branden/Fetch and API/NASA API/READNE.md
@@ -0,0 +1,2 @@
+Get your own API https://api.nasa.gov/
+and dont forget to put it in API_KEY
---
+++ JS with Branden/Fetch and API/NASA API/script.js
@@ -0,0 +1,28 @@
+let test = new Object()
+const seledka = document.getElementById("select")
+const bomba = document.getElementById("bomba")
+const begin = document.getElementById("begin")
+let date = "2024-07-31"
+
+
+h1 = begin.appendChild(document.createElement("h1"))
+explanation = bomba.appendChild(document.createElement("p"))
+image = bomba.appendChild(document.createElement("img"))
+
+
+seledka.addEventListener("change", function(e){
+    API_KEY = "Your API_key"
+    let bombaklat = e.target.value
+    console.log(bombaklat)
+    date = bombaklat
+    console.log(date)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
+   .then(response => response.json())
+   .then(data => {
+    test = data
+    h1.textContent = test.title
+    explanation.textContent = test.explanation
+    image.src = test.url
+
+   })
+})
