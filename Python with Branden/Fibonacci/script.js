bd0d00ba40c722a7598ee498c4397c04b649edd2
Te El <vladislavpe4@gmail.com>
2024-08-13 11:38:18 +0900

    commit

--- Python with Branden/Fibonacci/script.js
+++ Python with Branden/Fibonacci/script.js
@@ -3,7 +3,7 @@ let UserNum = prompt("What number do you want")
 for(i = 1; i < 20; i++){
     box[i+1] = box[i] + box[i-1]
     if( i == UserNum){
-        console.log(box[i])
+        console.log(box[i-1])
     }
 }
 console.log(box)
