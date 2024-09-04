83dd7febc280f17e0d81ba2ad7a38751607c6a87
Te El <vladislavpe4@gmail.com>
2024-09-04 10:33:47 +0900

    commit

---
+++ Python with Branden/September 2024/sorting/timing_sort_algo.py
@@ -0,0 +1,14 @@
+from timeit import repeat 
+# We re making bit by bit testing algorithm
+
+def run_sorting_algorithm(algorithm, list):
+    set_up_variable = f"from __main__ import {algorithm}" \
+        if algorithm != "sorted" else ""
+    
+    stmt = f"{algorithm}({list})"
+
+    repetitions = repeat(setup = set_up_variable, stmt = stmt, repeat = 3, number = 10)
+
+    
+    
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(repetitions)}")
